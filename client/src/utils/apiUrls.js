497262fb6e2a666c6483ab46ce7557fe289565ec
saronen1 <sole.aro@hotmail.com>
2022-05-26 11:44:08 +0300

    Add navigation, create Results view

--- client/src/utils/apiUrls.js
+++ client/src/utils/apiUrls.js
@@ -2,3 +2,4 @@ export const BASE_URL = 'http://localhost:8080';
 
 export const SEARCH_RESTAURANTS_BY_CITY_URL = '/api/v1/restaurants';
 export const VOTE_RESTAURANT_URL = '/api/v1/vote';
+export const TODAYS_VOTING_RESULTS_URL = '/api/v1/results';
---
+++ client/src/views/Results/Results.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { TODAYS_VOTING_RESULTS_URL } from '../../utils/apiUrls';
+import client from '../../utils/client';
+import { StatusCodes as HttpStatus } from 'http-status-codes';
+
+const getResults = async () => {
+	return client.get(TODAYS_VOTING_RESULTS_URL); 
+};
+
+const Results = () => {
+	const [results, setResults] = useState([]);
+
+	const updateResults = () => {
+		console.log('Fetching results!');
+		getResults()
+			.then((response) => {
+				if (response.status === HttpStatus.OK) {
+					setResults(response.data?.results);
+				} else {
+					// error occurred
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				// TODO: Snackbar: could not fetch results
+			});
+	};
+
+	// Fetches results immediately when view is rendered
+	useEffect(() => {
+		console.log('First useEffect');
+		updateResults();
+	}, []);
+
+	// Updates results every 10 seconds
+	useEffect(() => {
+		console.log('Second use effect');
+		setInterval(() => {
+			console.log('Another interval');
+			updateResults();
+		}, 10000);
+	}, []);
+
+	return (
+		<div>
+			<h1>Tulokset</h1>
+			<ul>
+				{results && results.map((result) => {
+					return(
+						<li key={result.restaurantId}>
+							{result.name}, ääniä: {result.votes}
+						</li>
+					);
+				})
+				} 
+			</ul>
+		</div>
+	);
+};
+
+export default Results;
