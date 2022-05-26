af89dbe8028f4a367e4af1b932d395afb9179644
saronen1 <sole.aro@hotmail.com>
2022-05-26 11:43:31 +0300

    Disable strict mode to avoid double renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import SearchRestaurants from './views/RestaurantSearch/SearchRestaurants';
+import Results from './views/Results/Results';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 	return (
-		<SearchRestaurants/>
+		<Router>
+			{/*  NAVBAR HERE */}
+			<Routes>
+				<Route path="/tulokset" element={<Results/>}/>
+				<Route path="/aanesta" element={<SearchRestaurants/>}/>
+				<Route path="*" element={<Navigate to="/aanesta"/>}/>
+			</Routes>
+		</Router>
 	);
 }
 
